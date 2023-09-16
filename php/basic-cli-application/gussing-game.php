341f444f8c5b2bd5333ea6f83a99dfe8a4eca511
chayandasgupta <chayandas285178@gmail.com>
2023-09-16 12:06:09 +0600

    modify code

--- php/basic-cli-application/gussing-game.php
+++ php/basic-cli-application/gussing-game.php
@@ -10,10 +10,15 @@
     }
     
     $number = rand($min, $max);
+
+    echo "================================================\n";
+    echo "      Welcome to the Number Guessing Game!\n";
+    echo "   I'm thinking of a number between $min and $max.\n";
+    echo "================================================\n\n";
     
     while(true) {
 
-      $input_number = readline("Enter your number: ");
+      $input_number = (int) readline("Enter your number: ");
       
       if($input_number > $number) {
         echo "Try a lower number.\n";
